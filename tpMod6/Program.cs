4e347ce23b032494fa3fd377b2b757386136b128
AdsGab <gabrielleadsf@gmail.com>
2024-03-24 12:38:48 +0700

    Menambahkan class SayaTubeVideo

--- tpMod6/Program.cs
+++ tpMod6/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+    Random rnd = new Random();
+
+    public SayaTubeVideo(string s) {
+        this.id = rnd.Next(10000,100000);
+        this.title = s;
+        this.playCount = 0;
+    }
+    public void IncreasePlayCount(int i) {
+        this.playCount = this.playCount + i;
+    }
+    public void PrintVideoDetails() {
+        Console.WriteLine("Judul video: " + this.title);
+        Console.WriteLine("ID Video: " + this.id);
+        Console.WriteLine("PlayCount video: " + this.playCount);
+    }
+}
+
+class porgram
+{
+    static void Main(String[] args)
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - Gabrielle");
+        video1.PrintVideoDetails();
+    }
+}
